4a51a627ec5e834122490e14b3c4dd343da8ab96
David Liang <David.Liang@nyu.edu>
2017-07-02 17:47:37 -0400

    added note on original function

--- Week3/ex2/sigmoid.m
+++ Week3/ex2/sigmoid.m
@@ -9,6 +9,7 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+% straightforward solution
 % g = 1 ./ (1 + exp(-z));
 
 % alternate solution using arrayfun
